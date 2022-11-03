87e8d270301265efa16f10e94b9721e848e26ea7
dionesh12 <81324187+dionesh12@users.noreply.github.com>
2022-11-03 10:31:23 +0530

    Removed the target directory

--- target/classes/com/dionesh/uitilites/SparkUtility.scala
+++
@@ -1,9 +0,0 @@
-package com.dionesh.uitilites
-
-import org.apache.spark.sql.SparkSession
-
-object SparkUtility {
-  
-    def createSparkSession(appName: String):SparkSession = SparkSession.builder.appName(appName).getOrCreate()
-    def readFile(spark: SparkSession, format: String, filePath: String) = spark.read.format(format).load(filePath)
-}
