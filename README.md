580e264cf87a45ec96c79d15cbe6beefd839f7fd
dionesh12 <ganeshbhat9187@gmail.com>
2022-11-03 10:29:18 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SparkETL-Scala
+## The project aims to provide an example of how a SparkETL can be written.
+
+The project is yet to begin.
+Current the following function is implemented.
+i.  Created a sparkSession.
+
+TODO
+ii. Created a generic read function
---
+++ target/classes/com/dionesh/uitilites/SparkUtility.scala
@@ -0,0 +1,9 @@
+package com.dionesh.uitilites
+
+import org.apache.spark.sql.SparkSession
+
+object SparkUtility {
+  
+    def createSparkSession(appName: String):SparkSession = SparkSession.builder.appName(appName).getOrCreate()
+    def readFile(spark: SparkSession, format: String, filePath: String) = spark.read.format(format).load(filePath)
+}
